e3f2879f8ffb8adf6718d807fde0f4018ffa35a7
Sam Hamburger <sh786@cornell.edu>
2023-02-28 07:25:43 -0700

    app dir

---
+++ app/api/config/route.ts
@@ -0,0 +1,8 @@
+export async function GET() {
+  const exampleConfig = {
+    title: 'TripIt',
+    description: 'An AI-based travel planner to help you explore and plan your next great trip.'
+  };
+
+  return Response.json({ data })
+}
